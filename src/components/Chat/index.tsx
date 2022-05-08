9a03ac5098809e7d0da2e9d3747f9f2ac5264f95
Almaz <diamond-4d@mail.ru>
2022-05-08 18:21:10 +0300

    realized add message

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection, doc, serverTimestamp, addDoc,
+} from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { Context } from '../..';
+import { ChatSendMessageForm } from './ChatSendMessageForm';
 
 export const Chat = () => {
+  const { auth, firestore } = useContext(Context);
+  const [user] = useAuthState(auth);
+  const [messages, loading] = useCollectionData(
+    collection(firestore, 'messages'),
+  );
+
+  const sendMessage = async (message: string) => {
+    try {
+      const docRef = await addDoc(collection(firestore, 'messages'), {
+        uid: user?.uid,
+        displayName: user?.displayName,
+        photoURL: user?.photoURL,
+        text: message,
+        createdAt: serverTimestamp(),
+      });
+      console.log(docRef);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>chat</div>
+    <div className='h-4/5 w-3/5 flex flex-col justify-between'>
+      <div className='h-3/5 border rounded-lg bg-white'>
+        Chat
+      </div>
+
+      <ChatSendMessageForm
+        sendMessage={sendMessage}
+      />
+    </div>
   );
 };
