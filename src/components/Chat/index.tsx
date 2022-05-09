4269ef4f253a5834953df8681b6a068715c08290
Almaz <diamond-4d@mail.ru>
2022-05-09 14:51:46 +0300

    update Chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -6,6 +6,8 @@ import {
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { Context } from '../..';
 import { ChatSendMessageForm } from './ChatSendMessageForm';
+import { ChatMessageItem } from './ChatMessageItem';
+import { IChatMessageItemProps } from '../../utils/types';
 
 export const Chat = () => {
   const { auth, firestore } = useContext(Context);
@@ -16,14 +18,13 @@ export const Chat = () => {
 
   const sendMessage = async (message: string) => {
     try {
-      const docRef = await addDoc(collection(firestore, 'messages'), {
+      await addDoc(collection(firestore, 'messages'), {
         uid: user?.uid,
         displayName: user?.displayName,
         photoURL: user?.photoURL,
         text: message,
         createdAt: serverTimestamp(),
       });
-      console.log(docRef);
     } catch (err) {
       console.log(err);
     }
@@ -31,13 +32,21 @@ export const Chat = () => {
 
   return (
     <div className='h-4/5 w-3/5 flex flex-col justify-between'>
-      <div className='h-3/5 border rounded-lg bg-white'>
-        Chat
+
+      <div className='h-3/5 border rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 p-3 overflow-auto'>
+        {messages?.map((message) => (
+          <ChatMessageItem
+            key={message.createdAt}
+            me={user?.uid === message.uid}
+            {...message as IChatMessageItemProps}
+          />
+        ))}
       </div>
 
       <ChatSendMessageForm
         sendMessage={sendMessage}
       />
+
     </div>
   );
 };
